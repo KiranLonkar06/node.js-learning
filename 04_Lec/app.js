a90588678b7d2b8de9c2e341ddad0f4f9df8b6e0
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-25 12:49:09 +0000

    Request via diffrent url

--- 04_Lec/app.js
+++ 04_Lec/app.js
@@ -3,14 +3,14 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
 
-  if (req.url === "/") {
+  if (req.url === "/abc") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<html><title>Welcome to home</title></html>");
     res.write("<body><h1>Like share subscibe</h1></body>");
     res.end();
     return ;
-  } else if (req.url === "/product") {
+  } else if (req.url === "/products") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<html><title>Complete code</title></html>");
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<html><title>Complete  code</title></html>");
-  res.write("<body><h1>Like share subscibe</h1></body>");
+  res.write("<body><h1>Kiran Lonkar</h1></body>");
   res.end();
 });
 const port = 3058;
