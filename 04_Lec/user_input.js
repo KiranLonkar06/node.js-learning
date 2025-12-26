ff8514116fab0a55ef4fb51e01faaef3969945e6
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-26 06:12:05 +0000

    user_input

--- 04_Lec/user_input.js
+++ 04_Lec/user_input.js
@@ -1,25 +1,40 @@
-const http = require('http');
+const http = require("http");
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
 
-const server = http.createServer((req,res)=>{
-  console.log(req.url,  req.method , req.headers);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<html><title>Enter your details : </title></html>");
 
-  if(req.url ==='/'){
-    res.setHeader('content-Type' , 'text/html');
-    RegExp.write('<html>');
-    res.write('<head><title>Complete coding</title></head>  ');
-    res.write('<body><h1>Enter your details: </h1>');
-    res.write('<form>');
-    res.write('input type="text" name ="username" placeholder="enter your name"');
-    
-    res.write('</form>');
+    res.write('<form action="/submit-details" method="POST">');
+
+    res.write('<input type ="text" name="username" placeholder="Enter your name"><br>');
+    res.write('<label for ="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male" />');
+    res.write('<label for ="female">Female</label>');
+    res.write('<input type="radio" id="female" name="gender" value="Female" />');
+    res.write('<br>')
+    res.write('<input type="submit" value="Submit"/>');
+    res.write('</form');
     res.write('</body>');
-    res.write('<html>');
-    return res.end();
+    res.end();
+    return ;
+  } else if(req.url.toLowerCase ==="/submit-details" && req.method == "POST")
+  {
+    fs.writeFileSync('user.txt' , 'Kiran Lonkar');
+    res.statusCode = 302;
+    re.setHeader( 'Location' , '/' );
   }
-  // res.setHeader('content-Type' , 'text/html');
-  //   RegExp.write('<html>');
-  //   res.write('<head><title>Complete coding</title></head>  ');
-  //   res.write('<body><h1>Welcome to Home</h1></body>');
-  //   res.write('<html>');
-  //   return res.end();
-})
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<html><title>Complete  code</title></html>");
+  res.write("<body><h1>Like share subscibe</h1></body>");
+  res.end();
+});
+const port = 3058;
+
+server.listen(port, () => {
+  console.log("server started");
+});
