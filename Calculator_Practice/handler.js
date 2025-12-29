bfd4dfa2c7ead251b3cb77a9b307e05334a5388d
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-29 11:30:34 +0000

    Calculator project

--- Calculator_Practice/handler.js
+++ Calculator_Practice/handler.js
@@ -1,7 +1,9 @@
-const requestHandler = (req, res)=>{
-  console.log(req.url , req.method);
-  if(req.url ==="/"){
-    res.setHeader('Content-Type', 'text/html');
+const { sumRequestHandler } = require("./sum");
+
+const requestHandler = (req, res) => {
+  console.log(req.url, req.method);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
     res.write(`
       <html>
        <head><title>Practice set</title></head>
@@ -11,22 +13,32 @@ const requestHandler = (req, res)=>{
        </body>
        </html>
       `);
-      return res.end();
-  }
-  else if (req.url.toLowerCase() === "/calculator") {
-  res.setHeader('Content-Type', 'text/html');
+    return res.end();
+  } else if (req.url.toLowerCase() === "/calculator") {
+    res.setHeader("Content-Type", "text/html");
     res.write(`
       <html>
        <head><title>Practice set</title></head>
        <body>
-       <h1>404 page does not found</h1>
-       <a href = "/">Go to home</a>
+       <h1>Here is the calculator</h1>
+       <form action ="/calculate-result" method="POST"> 
+        <input type = "text" placeholder="First Num" name="first"/>
+        <input type = "text" placeholder="Second Num" name="second"/>
+        <input type = "submit" value="Sum">
+        </form>
        </body>
        </html>
       `);
-      return res.end();
-}
-    res.setHeader('Content-Type', 'text/html');
+    return res.end();
+  } else if (
+    req.url.toLowerCase() === "/calculate-result" &&
+    req.method === "POST"
+  ) {
+    sumRequestHandler(req, res);
+    return ;
+  }
+
+  res.setHeader("Content-Type", "text/html");
   res.write(`
     <html>
       <head><title>Practise Set</title></head>
@@ -34,10 +46,10 @@ const requestHandler = (req, res)=>{
         <h1>404 Page Does not Exist</h1>
         <a href="/">Go To Home</a>
       </body>  
-    <html>  
-  `);    
+    </html>  
+  `);
   return res.end();
-}
+};
 console.log("Request reached server");
 
 exports.requestHandler = requestHandler;
