b1ea274401671b405d296e5c629b7277def5e56a
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-30 05:15:17 +0000

    Error and debugging

---
+++ Practice_set /app.js
@@ -0,0 +1,14 @@
+function calculateArea(width ,height){
+    return width+height;
+}
+let width = 10;
+let height = 5;
+let area = width *height*width;
+if(area > 100){
+    console.log("the area is large");
+}else{
+    console.log("the area is small ");
+}
+if(width +height >100){
+    console.log("area is greater than or equal to 100");
+}
