03ebcf20cb0df791a999f5c79801458a383b3f0a
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-30 15:32:33 +0000

    Practise set express

---
+++ Practice_set_express/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("First dummy middle ware", req.url,req.method);
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("second dummy middle ware",req.url,req.method);
+    next();
+});
+
+// app.use((req ,res ,next)=>{
+//     console.log("third middle ware",req.url,req,method);
+//     res.send("<h1>Welcome to complete coding</h1>")
+// });
+
+app.get("/",(req,res,next)=>{
+    console.log("handling slash for get ",req.url,req.method);
+    res.send(`<h1>Welcome to complete coding </h1>`)
+    
+});
+
+app.get("/contact-us",(req,res,next)=>{
+    console.log("handling contact us for get ",req.url,req.method);
+    res.send(`
+        <h1>plz give your details </h1>
+        <form action ="/contact-us" method="POST">
+            <input type="text" name="name" placeholder="Enter your name"/>
+            <input type="email" name="email" placeholder="Enter your email"/>
+            <input type="Submit" />
+        </form>
+        `)  
+    next();
+});
+
+app.post("/contact-us",(req,res,next)=>{
+    
+})
+
+const PORT = 3002;
+app .listen(PORT , ()=>{
+    console.log(`server running on address http://localhost:${PORT}`);
+})
